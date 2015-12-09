5d3b0b43e87eb49dac526d5a6ddf43b317a5bfbc
Nicolas <nshihmen@gmail.com>
2015-12-09 16:03:00 +0100

    Ajout du $_GET dans AddEntry pour pouvoir ajouter un élément dans le tableau correspondant de la base de donnée

--- PHP_Files/AddEntry.php
+++ PHP_Files/AddEntry.php
@@ -5,15 +5,23 @@
  * Date: 12/9/2015
  * Time: 11:56 AM
  */
-try {
-    $huby_db = new PDO(
-        'sqlite:HubyDb.db');
-    $huby_db->setAttribute(PDO::ATTR_ERRMODE,
-        PDO::ERRMODE_EXCEPTION);      // Select all data from file db messages table
+if (isset($_GET['flow'])) {
+    try {
 
-    $result = $huby_db->query("INSERT INTO Entry(Date)
-                                  VALUES (datetime('now'))");
-}
-catch(PDOException $e){
-    echo $e->getMessage();
+
+        $huby_db = new PDO(
+            'sqlite:HubyDb.db');
+        $huby_db->setAttribute(PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION);      // Select all data from file db messages table
+
+        if ($_GET['flow'] == 1) {
+            $result = $huby_db->query("INSERT INTO Entry(Date)
+                                  VALUES (datetime('now','+1 hour'))");
+        } else if ($_GET['flow'] == 0) {
+            $result = $huby_db->query("INSERT INTO Exit(Date)
+                                  VALUES (datetime('now','+1 hour'))");
+        }
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+    }
 }
