a7cf964b1a19dd2f58d0bec1f7b14b8b7d87b535
Nicolas <nshihmen@gmail.com>
2016-01-20 14:47:49 +0100

    Ajout du $_GET dans AddEntry pour pouvoir ajouter un élément dans le tableau correspondant de la base de donnée

--- PHP_Files/SelectedPeriodStats.php
+++ PHP_Files/SelectedPeriodStats.php
@@ -5,25 +5,47 @@
  * Date: 12/9/2015
  * Time: 4:16 PM
  */
+$dateStart = null;
+$dateEnd = null;
 
-$huby_db = new PDO(
-    'sqlite:HubyDb.db');
-$huby_db->setAttribute(PDO::ATTR_ERRMODE,
-    PDO::ERRMODE_EXCEPTION);
+function checkDateFormat()
+{
+    global $dateStart;
+    global $dateEnd;
 
-//$range_start = $_GET["start"];
-//$range_end = $_GET["end"];
+    $dateStart = $_GET["start"];
+    $dateEnd = $_GET["end"];
 
-$datetime1 = new DateTime('2012-01-01');
-$datetime2 = new DateTime('2014-01-01');
 
-$difference = $datetime2->diff($datetime1);
-echo $difference->format('%y months');
-echo ("\r\n");
 
-$PDOresult = $huby_db->query("SELECT COUNT(*) as total from Entry where Date between '2012-01-01' and '2016-01-01'");
+    $englishDate = "/^([0-9]{4})-([0-9]{2})-([0-9]{2})/";
+    $frenchDate = "/^([0-9]{2})-([0-9]{2})-([0-9]{4})/";
+    if (preg_match($englishDate, $dateStart, $matches) != null) {
+        return true;
+    } else if (preg_match($frenchDate, $dateStart, $matches) != null) {
+        $dateStart = $matches[3]."-".$matches[2]."-".$matches[1];
+        preg_match($frenchDate, $dateEnd, $matches);
+        $dateEnd = $matches[3]."-".$matches[2]."-".$matches[1];
+        return true;
+    } else {
+        return false;
+    }
+}
 
-$result = $PDOresult->fetch();
 
-echo $result['total'];
+$validDate = checkDateFormat();
+if($validDate == true) {
+    $huby_db = new PDO(
+        'sqlite:HubyDb.db');
+    $huby_db->setAttribute(PDO::ATTR_ERRMODE,
+        PDO::ERRMODE_EXCEPTION);
+
+    $queryString = "SELECT * from Entry where Date between '".$dateStart."' and '".$dateEnd."'";
+
+    $PDOresult = $huby_db->query($queryString);
+
+    $result = $PDOresult->fetch();
+
+    echo $result[0];
+}
 
