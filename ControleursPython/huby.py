769d1b5c05212cb3f3ba9bea09351d6767efdffb
TheGerbille <gerbilledu0138@gmail.com>
2016-04-27 17:06:34 +0200

    Probleme lag + extiction des leds pt2

--- ControleursPython/huby.py
+++ ControleursPython/huby.py
@@ -19,9 +19,13 @@ GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 ############
 
 #Leds
+GPIO.setwarnings(False)
 GPIO.setup(23, GPIO.OUT)
 GPIO.setup(24, GPIO.OUT)
 GPIO.setup(25, GPIO.OUT)
+GPIO.output(23, GPIO.LOW)
+GPIO.output(24, GPIO.LOW)
+GPIO.output(25, GPIO.LOW)
 
 #RFID
 port = serial.Serial(config["SERIAL"]["Interface"], baudrate=9600)
