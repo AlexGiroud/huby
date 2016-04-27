0b0ab3a3832b9cb30bfe57b4f27d3c6c7042a0ce
AlexGiroud <gerbilledu0138@gmail.com>
2016-04-27 09:40:55 +0000

    Modifications depuis le rapsi du controleur python

--- ControleursPython/testSerial.py
+++ ControleursPython/testSerial.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import serial
-import urllib
+import http.client, urllib.request
 from time import localtime, strftime
-#import re
+import re
 import time
 import configparser
 
@@ -11,24 +11,26 @@ config.read('config.ini')
 
 port = serial.Serial(config["SERIAL"]["Interface"], baudrate=9600, timeout=3.0)
 #port = serial.Serial("ttyAMA0", baudrate=9600, timeout=3.0)
-prog = re.compile("^[A-Z0-9a-z]{10}$")
+prog = re.compile("^[A-Z0-9a-z]{12}$")
 
 while True:
-    rcv = port.read(10)
-    recivedString = rcv.decode('utf-8').replace('\n','').replace('\r','').replace('\r\n','')
+    rcv = port.read(14)
+    recivedString = rcv.decode('utf-8')
+    recivedString = recivedString[1:-1]
     print(recivedString)
-    if prog.find(recivedString):
+    if prog.match(recivedString):
         print("matched")
-        params = urllib.urlencode({'flow': 'in'})
-        headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
-        conn = httplib.HTTPConnection(config["SERVER"]["Url"]+"AddEntry.php:80")
-        try:
-            conn.request("GET", "", params, headers)
-            response = conn.getresponse()
-            data = response.read()
-            conn.close()
-        except:
-            print "Connection failed"
+        urllib.request.urlopen(config["SERVER"]["Url"]+"AddEntry.php?flow=in")
+        #params = urllib.parse.urlencode({'flow': 'in'})
+        #headers = {"Content-type": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        #conn = http.client.HTTPConnection(config["SERVER"]["Url"]+"AddEntry.php:80")
+        #try:
+        #    conn.request("GET", "", params, headers)
+        #    response = conn.getresponse()
+        #    data = response.read()
+        #    conn.close()
+        #except:
+        #    print("Connection failed")
         #        test = urllib.urlopen(config["SERVER"]["Url"]+"action.controller.php?action=motion")
         time.sleep(1)
         port.flushInput()
